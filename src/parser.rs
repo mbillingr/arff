ae5aea96aa960625678c74ca51b48f6d23b27b95
mbilling <martin.billinger@medel.com>
2018-05-22 16:33:42 +0200

    Updated license

--- src/parser.rs
+++ src/parser.rs
@@ -1,3 +1,11 @@
+// Copyright 2018 Martin Billinger
+//
+// Licensed under the Apache License, Version 2.0 <LICENSE-APACHE or
+// http://www.apache.org/licenses/LICENSE-2.0> or the MIT license
+// <LICENSE-MIT or http://opensource.org/licenses/MIT>, at your
+// option. This file may not be copied, modified, or distributed
+// except according to those terms.
+
 use std::{i16, i32, i64, u8, u16, u32, u64, f64};
 
 use super::error::{Error, Result};
